4fab3c3875b54aea801401ac528029e8b1dd45b4
Kyoko <kyokoconnect@gmail.com>
2021-03-21 17:09:08 -0400

    1st commit, not done yet

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here!
+const element = document.createElement('div');
+document.body.appendChild(element);
+
+
+document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id ="Victory"; 
+
+
+// const header = document.getElementById('header');
+
+// const ul = document.createElement('ul');
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement('li');
+//   li.innerHTML = (i + 1).toString();
+//   ul.appendChild(li);
+
+//   element.appendChild(ul);
+
+
+// element.style.backgroundColor = '#27647B';
+// element.style.fontSize = '24px';
+// element.style.marginLeft = '30px';
+// element.style.lineHeight = 2;
+// element.classList.remove("unneeded-class");
+// element.classList.add("pet-listing", "dog");
+
+
+
+
+
