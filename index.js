99dcb75d9c9be21635b56c91c7085ac2f629745e
Kyoko <kyokoconnect@gmail.com>
2021-03-23 11:40:01 -0400

    AFter tried all, not passing 3

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-const element = document.createElement('div');
-document.body.appendChild(element);
+document.querySelector('main'); {
+main.remove()
+}
 
+// it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//     expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+// });
+document.createElement('h1'); {
+    const newHeader = 'H1';
+}
 
-document.querySelector('main');
-main.remove();
+// it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//     expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+//   });
 
-const newHeader = document.createElement('h1');
-newHeader.id ="Victory"; 
+document.createElement('h1#victory'); {
+    H1.id = 'victory';
+}
 
+// it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+//     expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+//   });
 
-// const header = document.getElementById('header');
-
-// const ul = document.createElement('ul');
-
-// for (let i = 0; i < 3; i++) {
-//   const li = document.createElement('li');
-//   li.innerHTML = (i + 1).toString();
-//   ul.appendChild(li);
-
-//   element.appendChild(ul);
-
-
-// element.style.backgroundColor = '#27647B';
-// element.style.fontSize = '24px';
-// element.style.marginLeft = '30px';
-// element.style.lineHeight = 2;
-// element.classList.remove("unneeded-class");
-// element.classList.add("pet-listing", "dog");
-
-
-
-
-
+document.createElementById('h1#victory'); {
+    body.H1.innerHTML = 'Kyoko is the champion';
+}
