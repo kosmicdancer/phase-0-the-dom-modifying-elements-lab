4127e234c16298e87b46921141bf8d5aa9aea723
Kyoko <kyokoconnect@gmail.com>
2021-04-22 03:05:10 -0400

    revisit

--- index.js
+++ index.js
@@ -5,22 +5,18 @@ main.remove()
 // it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
 //     expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
 // });
-document.createElement('h1'); {
-    const newHeader = 'H1';
-}
+const newHeader = document.createElement('h1'); 
 
 // it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
 //     expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
 //   });
 
-document.createElement('h1#victory'); {
-    H1.id = 'victory';
-}
+newHeader.id = ('victory');
+
 
 // it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
 //     expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
 //   });
 
-document.createElementById('h1#victory'); {
-    body.H1.innerHTML = 'Kyoko is the champion';
-}
+newHeader.innerHTML = ('Kyoko is the champion');
+
